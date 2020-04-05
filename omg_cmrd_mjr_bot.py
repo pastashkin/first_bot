6e4ab8d31e37604a6d21072ba13529719f18a0ef
Pavel Astashkin <pastashkin@gmail.com>
2020-04-05 20:07:27 +0300

    Answer w/o replying

--- omg_cmrd_mjr_bot.py
+++ omg_cmrd_mjr_bot.py
@@ -5,10 +5,10 @@ bot = telebot.TeleBot(os.environ.get('TELEGRAM_BOT_TOKEN'))
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Wtf? Wanna talk?")
+	bot.reply_to(message, "Привет!")
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+	bot.reply_to(message, "Это и так понятно...")
 
 bot.polling()
