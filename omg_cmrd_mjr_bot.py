b4c8d6a3ed33841cb658544ca7f1ec82377bda5e
Pavel Astashkin <pastashkin@gmail.com>
2020-04-05 21:22:23 +0300

    Smart answer

--- omg_cmrd_mjr_bot.py
+++ omg_cmrd_mjr_bot.py
@@ -3,12 +3,17 @@ import os
 
 bot = telebot.TeleBot(os.environ.get('TELEGRAM_BOT_TOKEN'))
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Привет!")
+	bot.send_message(message, "Привет! Я пока умею отвечать приветом на привет и говорить что делать.")
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-	bot.reply_to(message, "Это и так понятно...")
+	if message.text == "Привет!":
+	    bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+	elif message.text == "/help":
+	    bot.send_message(message.from_user.id, "Напиши привет")
+	else:
+	    bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")	
 
-bot.polling()
+bot.polling(none_stop=True, interval=1)
